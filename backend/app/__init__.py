266a9f0cd5f1ff1ee2c65418cf3ea1ea59fe8928
usernelson <nelson.valenzuelac@icloud.com>
2025-03-31 20:48:32 -0300

    primer update

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app, origins=["https://ia.agroup.app"])
+    CORS(app,
+     origins=["https://ia.agroup.app", "https://dev.ia.agroup.app"],
+     supports_credentials=True,
+     methods=["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+     allow_headers=["Content-Type", "Authorization"]
+)
 
     app.register_blueprint(routes)
 
